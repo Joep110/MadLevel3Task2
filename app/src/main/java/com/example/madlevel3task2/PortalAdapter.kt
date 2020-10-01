a1dd26598f1a006df35704513586c4137ad83fa4
Joep Boekhold <joepboekhold@gmail.com>
2020-10-01 17:25:44 +0200

    Added adapter and changed navcontroller to MainActivity.kt

--- app/src/main/java/com/example/madlevel3task2/PortalAdapter.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalAdapter.kt
@@ -1,27 +1,30 @@
 package com.example.madlevel3task2
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_portal.view.*
 
 class PortalAdapter(private val portals: List<Portal>) : RecyclerView.Adapter<PortalAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
-        fun databind(reminder: Portal) {
-
+        fun databind(portal: Portal) {
+            itemView.tvPortalTitle.text = portal.portalTitle
+            itemView.tvPortalUrl.text = portal.portalLink
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PortalAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_portal, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return portals.size
     }
 
     override fun onBindViewHolder(holder: PortalAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.databind(portals[position])
     }
 }
