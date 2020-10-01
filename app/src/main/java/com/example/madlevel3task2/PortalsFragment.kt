66f9bbd41dac327553e1a6abaebc87be6632e7b8
Joep Boekhold <joepboekhold@gmail.com>
2020-10-01 17:41:49 +0200

    Added initViews for portalFragment and added a recyclerView to fragment_portals.xml

--- app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
@@ -6,12 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_portals.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class PortalsFragment : Fragment() {
+
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +31,13 @@ class PortalsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        rvPortals.layoutManager =
+            GridLayoutManager(context, 2)
+        rvPortals.adapter = portalAdapter
+        portalAdapter.notifyDataSetChanged()
     }
 }
